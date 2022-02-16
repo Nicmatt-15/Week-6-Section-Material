ccb2245153bbaedda68d9b0dfc3ab42f42701b4c
Nicolas Matthew Suhardi <81585321+Nicmatt-15@users.noreply.github.com>
2022-02-16 13:46:31 -0800

    Add files via upload

---
+++ Baby A6 Kit/BabyA6.java
@@ -0,0 +1,25 @@
+/*
+* The skeleton code is written by Nicolas Matthew Suhardi
+* Please do not distribute unless given the permisison to do so
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class BabyA6 {
+    public static void main(String[] args) {
+        /*
+         * Do not alter the code creating the WordRandomizer object
+         *
+         * Documentation:
+         * .generateWord() takes in no parameter and returns a String of a random
+         * adjective/noun/verb
+         *
+         * Example:
+         * String someWord = wordGenerator.generateWord();
+         */
+        WordRandomizer wordGenerator = new WordRandomizer();
+
+        // Your code below
+    }
+}
